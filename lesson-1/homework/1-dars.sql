bd8d8a70fa876f5277fb6a9f60d64ab304dc7380
ilhomasqarov <ilkhomas2000@gmail.com>
2025-04-17 22:34:23 -0700

    Create 1-dars.sql

---
+++ lesson-1/homework/1-dars.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+);
+
+insert into Students values ('1', 'ilhom', '23')
+insert into Students values ('2', 'yusuf', '1')
+insert into Students values ('3', 'komil', '55');
+
